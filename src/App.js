4a775a84d97a456a75ebec1eee64fe5c9b72a700
sanke reddiah <sankereddiah@gmail.com>
2024-08-12 16:19:29 +0530

    new Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { Component } from "react"
-class App extends Component{
-    state={count:0}
-render(){
-    let{count}=this.state
-    return(
-        <div>
-<button onClick={()=>(this.setState(prevState=>({count:prevState.count-1})))}>
-    decrease
-</button>
-<h1>{count}</h1>
-<button onClick={()=>(this.setState(prevState=>({count:prevState.count+1})))}>
-    increase
-</button>
-        </div>
-    )
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import SignUp from './components/Signup';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/signup" replace />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
